096eb00a11f21369c729f767b7f84bc55fbc7147
brianxor <brianiter2@gmail.com>
2024-10-05 20:10:59 +0300

    changed /handle endpoint to /forward

--- server/server.go
+++ server/server.go
@@ -18,7 +18,7 @@ func StartServer(serverHost string, serverPort string) error {
 	tlsGroup.Use(logger.New())
 	tlsGroup.Use(recover.New())
 
-	tlsGroup.Post("/handle", handlers.HandleTlsRoute)
+	tlsGroup.Post("/forward", handlers.HandleTlsForwardRoute)
 
 	serverAddress := fmt.Sprintf("%s:%s", serverHost, serverPort)
 
